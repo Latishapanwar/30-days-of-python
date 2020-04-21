ad9209e3d99ab236057e2e2d14b9acd648422641
Latisha Panwar <latishapanwar@gmail.com>
2020-04-21 17:12:43 +0100

    day4 and 5 completed

---
+++ day5-loops.py
@@ -0,0 +1,8 @@
+'''Task
+Given an integer, n, print its first 10 multiples. Each multiple nxi should be
+printed on a new line in the form: n x i = result.'''
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, 11):
+        print("{} x {} = {}".format(n, i, n*i))
