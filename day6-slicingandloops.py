8aaa25f821fecf3a9422f0002224027843ae0622
Latisha Panwar <latishapanwar@gmail.com>
2020-04-24 00:46:10 +0100

    day6 completed

---
+++ day6-slicingandloops.py
@@ -0,0 +1,8 @@
+'''Task
+Given a string, S, of length N that is indexed from 0 to N-1, print its
+even-indexed and odd-indexed characters as 2 space-separated strings on a single
+line (see the Sample below for more detail).'''
+
+for N in range(int(input())):
+    S = input()
+    print(S[::2], S[1::2])
